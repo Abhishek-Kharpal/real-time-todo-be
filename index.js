261a2316101339f6bf0c81b8ef01d39b1c58b3c6
Abhishek <abhikharpal@gmail.com>
2023-03-26 21:49:45 +0530

    feat: add sockets to handle add todo

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const morgan = require('morgan');
 const todoRoutes = require('./src/routes/todo');
 const app = express();
 const port = 3001;
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on(
+    'connection', (socket)=>{
+      console.log('user connected');
+      socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+    },
+);
 
 app.use(cors());
 app.use(morgan('dev'));
@@ -11,6 +27,9 @@ app.use(express.json());
 
 app.use('/api/todos', todoRoutes);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+const socketIoObject = io;
+module.exports.ioObject = socketIoObject;
+
--- src/controllers/todo.js
+++ src/controllers/todo.js
@@ -1,5 +1,5 @@
 const todoService = require('../services/todo');
-
+const socketIoObject = require('../../index');
 const getAllTodos = async (req, res) => {
   try {
     const todos = await todoService.getAllTodos();
@@ -20,13 +20,10 @@ const getTodo = async (req, res) => {
 };
 
 const createTodo = async (req, res) => {
-  try {
-    const {title, content} = req.body;
-    const todo = await todoService.createTodo(title, content);
-    res.status(201).json(todo);
-  } catch (err) {
-    res.status(500).json({error: err.message});
-  };
+  const {title, content} = req.body;
+  const todo = await todoService.createTodo(title, content);
+  socketIoObject.ioObject.emit('addTodo', todo);
+  res.status(201).json(todo);
 };
 
 const updateTodo = async (req, res) => {
@@ -34,6 +31,7 @@ const updateTodo = async (req, res) => {
     const {title, content} = req.body;
     const id = req.params.id;
     const todo = await todoService.updateTodo(id, title, content);
+    socketIoObject.ioObject.emit('updateTodo', todo);
     res.status(200).json(todo);
   } catch (err) {
     res.status(500).json({error: err.message});
@@ -44,6 +42,7 @@ const deleteTodo = async (req, res) => {
   try {
     const id = req.params.id;
     await todoService.deleteTodo(id);
+    socketIoObject.ioObject.emit('deleteTodo', id);
     res.status(204).json();
   } catch (err) {
     res.status(500).json({error: err.message});
